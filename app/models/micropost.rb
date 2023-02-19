ec9c1da1f91c68ac59af8d9349775ae13f1cf00c
ktp <nkttkn666@gmail.com>
2023-02-19 03:20:01 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :favorites　 #foreign_key: 'micropost_id'はいらない?
+
   
   validates :content, presence: true, length: { maximum: 255 }
 end
